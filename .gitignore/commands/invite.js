e326f06d75aa7e9020b48dbb032b2db3e24e052d
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-05-24 23:01:05 +0200

    Add files via upload

---
+++ .gitignore/commands/invite.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send(`You can invite me with this link:\nhttps://discordapp.com/api/oauth2/authorize?client_id=${429745888152518671}&permissions=51200&scope=bot`)
+}
